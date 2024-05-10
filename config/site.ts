797124665a2f35a378d0f093f9214380649f1964
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-10 19:26:48 +0300

    setup config project (title, description, logo ,global style, fonts, theme)

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Microtech",
+  description:
+    "At MicroTech, we're dedicated to transforming your ideas into powerful digital experiences. As a leading application design and development company, we specialize in creating customized, cutting-edge solutions that propel your business forward. Discover Our Expertise",
+};
