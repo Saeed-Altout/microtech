7809789d249b27cc2764546b3d671cd67d8b0dea
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-11 16:06:31 +0300

    create modal component

---
+++ components/ui/modal.tsx
@@ -0,0 +1,41 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const Modal = ({
+  title,
+  description,
+  children,
+  isOpen,
+  onClose,
+}: ModalProps) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
