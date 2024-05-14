17baa11a17055ea9e86b4e91b77cf14e9caef761
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-14 14:06:08 +0300

    update description contact-us page

--- app/(root)/(routes)/_contact-us/page.tsx
+++ app/(root)/(routes)/_contact-us/page.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Contact Us",
-  description: "Contact Us Page",
+  description:
+    "Get in touch with MicroTech, your dedicated partner in transforming ideas into powerful digital experiences. As a leading application design and development company, we specialize in creating customized, cutting-edge solutions to propel your business forward. Reach out to us today to discover how our expertise can benefit you.",
 };
 
 export default function ContactUsPage() {
